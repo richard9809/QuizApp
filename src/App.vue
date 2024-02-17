39f110e55ece9a0ecb3b7cc072af5669a0e42f54
Richard Mulu <richard.ndisya@strathmore.edu>
2024-02-17 13:50:19 +0300

    add search logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
 import q from "./data/quizes.json";
-import { ref } from "vue";
+import { ref, watch  } from "vue";
 
 const quizes = ref(q);
+const search = ref("")
+
+watch(search, () => {
+  quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+})
+
 </script>
 
 <template>
   <div class="container">
     <header>
       <h1>Quizes</h1>
-      <input type="text" placeholder="Search..." />
+      <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
       <div v-for="quiz in quizes" :key="quiz.id" class="card">
