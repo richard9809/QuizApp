d53613950b98d0384276e35b12df79ace9bad094
Richard Mulu <richard.ndisya@strathmore.edu>
2024-02-17 13:36:51 +0300

    added template and style structure

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,80 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import q from "./data/quizes.json";
+import { ref } from "vue";
+
+const quizes = ref(q);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" placeholder="Search..." />
+    </header>
+    <div class="options-container">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img
+          :src="quiz.img"
+          alt="Card Image"
+        />
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} questions</p>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
 header {
-  line-height: 1.5;
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+/* Card */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
 }
 </style>
