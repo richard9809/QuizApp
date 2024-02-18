4ab254d9151078741456aa32584cc5f2f4860e76
Richard Mulu <richard.ndisya@strathmore.edu>
2024-02-18 11:06:15 +0300

    Added result and quizheader component

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,13 +1,63 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import Result from "../components/Result.vue";
+import { useRoute } from "vue-router";
+import quizes from "../data/quizes.json";
+import { ref, watch, computed } from "vue";
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
+
+// const questionStatus = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+
+// watch(() => currentQuestionIndex.value, () => {
+//   questionStatus.value = `${currentQuestionIndex.value}/${quiz.question.length}`
+// })
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
 </script>
 
 <template>
   <div>
-    <QuizHeader />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
-      <Question />
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
+    {{ numberOfCorrectAnswers }}
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
